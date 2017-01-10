57966ca451692fd52e4fddf0a8ae636cd5ec43b8
Greg Konradt <greg.k@taptica.com>
2017-01-10 17:07:45 +0200

    A few minor changes in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'users'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+name 'simple_users'
+maintainer 'Greg Konradt'
+maintainer_email 'grifkon+github@gmail.com'
 license 'all_rights'
-description 'Installs/Configures users'
-long_description 'Installs/Configures users'
+description 'Manages Ubuntu/CentOS local users'
+long_description 'Manages local user accounts including adding and removing them from sudoers. Usernames and other details are defined in attributes.'
 version '0.1.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -16,4 +16,4 @@ version '0.1.0'
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/users' if respond_to?(:source_url)
+# source_url 'https://github.com/grifonas/simple_users' if respond_to?(:source_url)
