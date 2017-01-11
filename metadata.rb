335c3b832b03b5a82bf455612a4d9a5bd7ea0dee
Greg Konradt <greg.k@taptica.com>
2017-01-11 09:18:35 +0200

    Version change

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grifkon+github@gmail.com'
 license 'Apache v2.0'
 description 'Manages Linux (Ubuntu/CentOS) local users'
 long_description 'Manages local user accounts including adding and removing them from sudoers. Usernames and other details are defined in attributes.'
-version '0.1.3'
+version '0.1.4'
 
 %w( ubuntu debian redhat centos ).each do |os|
   supports os
