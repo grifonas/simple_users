8a634f59d7e037c51fee18ab0dbda3f8dd925f3e
Greg Konradt <greg.k@taptica.com>
2017-01-11 09:52:47 +0200

    Corrected the supported platforms section

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'grifkon+github@gmail.com'
 license 'Apache v2.0'
 description 'Manages Linux (Ubuntu/CentOS) local users'
 long_description 'Manages local user accounts including adding and removing them from sudoers. Usernames and other details are defined in attributes.'
-version '0.1.4'
+version '0.1.5'
 
-%w( ubuntu debian redhat centos ).each do |os|
+%w( ubuntu centos ).each do |os|
   supports os
 end
 
